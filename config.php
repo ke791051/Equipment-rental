89bc322a35df4e78ea84c3e76f2fed67d4caf4d8
Yuo <s11977037@gms.nutc.edu.tw>
2013-07-12 14:10:53 +0800

    新增SITE_TITLE設定

--- config.php
+++ config.php
@@ -4,4 +4,5 @@ $config = array();
 $config['BASE_PATH'] = 'http://localhost:1628/htdocs/';
 $config['APP_PATH'] = realpath('.') . '/';
 $config['CSS_HREF'] = $config['BASE_PATH'] . 'css/style.css';
+$config['SITE_TITLE'] = '資訊管理系設備出借系統';
 // End of file
