3e2a6ef050810edf28874d0d423c152b3381bc06
Yuo <s11977037@gms.nutc.edu.tw>
2013-07-12 13:52:48 +0800

    修改APP_PATH設定   - 增加尾端斜線

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 $config = array();
 
 $config['BASE_PATH'] = 'http://localhost:1628/htdocs/';
-$config['APP_PATH'] = realpath('.');
+$config['APP_PATH'] = realpath('.') . '/';
 $config['CSS_HREF'] = $config['BASE_PATH'] . 'css/style.css';
 // End of file
