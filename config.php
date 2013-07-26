aeb78935e882e151a9c4b8179a5c6dc265e31416
Yuo <s11977037@gms.nutc.edu.tw>
2013-07-26 18:28:48 +0800

    - 修改connection使用方式 - 修改載入路徑設定

--- config.php
+++ config.php
@@ -5,4 +5,6 @@ $config['BASE_PATH'] = 'http://localhost:1628/htdocs/';
 $config['APP_PATH'] = realpath('.') . '/';
 $config['CSS_HREF'] = $config['BASE_PATH'] . 'css/style.css';
 $config['SITE_TITLE'] = '資訊管理系設備出借系統';
+
+set_include_path(get_include_path() . PATH_SEPARATOR . $config['APP_PATH']);
 // End of file
