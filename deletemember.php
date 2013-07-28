249a66e3f5dcfc0acc1ad6e86a298927294807fd
Yuo <s11977037@gms.nutc.edu.tw>
2013-07-28 17:16:51 +0800

    看來是要重新製作了，刪除學長姐的舊檔案

--- deletemember.php
+++
@@ -1,22 +0,0 @@
-<?php session_start(); ?>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-<?php
-
-include("mysql_connect.inc.php");
-
-	$id = $_GET["id"];
-	$query = sprintf("DELETE FROM madata WHERE id='%s'", $id);
-	mysqli_query($db, $query) or die(mysqli_error());
-	header("Location:updata2.php");
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>無標題文件</title>
-</head>
-
-<body>
-</body>
-</html>
--- logout.php
+++
@@ -1,10 +0,0 @@
-<?php
-	if (!isset($_SESSION))
-	{
-		session_start();
-	}
-	unset($_SESSION["account"]);
-	unset($_SESSION["name"]);
-	unset($_SESSION["permission"]);
-	header("Location: ../index.php");
-?>
