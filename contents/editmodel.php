ed47c4a591a301d1db4a9ca76d6b4365a7632d2b
Yuo <s11977037@gms.nutc.edu.tw>
2013-07-28 09:06:54 +0800

    - 新增編輯model view - 新增newmodel controller - 新增管理者導覽頁樣版

---
+++ contents/editmodel.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ *  anticipate:
+ *    - array $modelData key為對應的資料庫欄位名稱
+ *    - string $submitValue '新增設備型號'、'更新設備型號'等等之類的值
+ *    - string $postUrl
+ *    - string $postDeleteImageUrl
+ *    - array|NULL $image array('id' => imageDatabaseId, 'name' => imageName, 'path' => imagePath)
+ *    - array $errors
+ *    - array $infos
+ *    - array $categories 符合Model回傳資料規範
+ *   
+ */
+?>
+<h1><?php echo $submitValue ?></h1>
+
+<ul>
+	<?php foreach($errors as $error): ?>
+		<li><?php echo $error ?></li>
+	<?php endforeach ?>
+</ul>
+
+<ul>
+	<?php foreach($infos as $info): ?>
+		<li><?php echo $info ?></li>
+	<?php endforeach ?>
+</ul>
+
+<form action="<?php echo $postUrl ?>" method="post" enctype="multipart/form-data" />
+	<ol>
+		<li><label>設備型號：<input type="text" name="model" value="<?php echo $modelData['model']?>" /></label></li>
+		<li>
+			<label>
+				設備種類：
+				<select name="category_id" required>
+					<option value="">請選擇設備種類</option>
+					<?php foreach($categories as $category): ?>
+						<option value="<?php echo $category['id'] ?>" <?php echo $modelData['category_id'] == $category['id'] ? 'selected' : '' ?>><?php echo $category['name'] ?></option>
+					<?php endforeach; ?>
+				</select>
+			</label>
+		</li>
+		<li>設備圖片:<input type="file" name="image" /></li>
+		<li><input type="submit" value="<?php echo $submitValue?>"></li>
+	</ol>
+</form>
+
+<!-- Delete Image Form -->
+<?php if (!is_null($image)): ?>
+<form action="<?php echo $postDeleteImageUrl ?>" method="post" >
+	<ol>
+		<li>
+			<img src="<?php echo $image['path'] ?>" alt="<?php echo $image['name'] ?>" />
+			<input type="hidden" name="delete_image_id" value="<?php echo $image['id'] ?>" />
+			<input type="submit" value="刪除圖片" />
+		</li>
+	</ol>
+</form>
+<?php endif ?>
---
+++ newmodel.php
@@ -0,0 +1,89 @@
+<?php
+	require_once 'config.php';
+    // 驗證使用者權限
+    $authSystem = new AuthSystem();
+	$loginSystem = new LoginSystem();
+	
+	$loginUserRank = $loginSystem->getLoginUserRank();
+    // 未登入使用者，Show404
+    if (is_null($loginUserRank)) {
+    	$authSystem->show404();
+		exit();
+    }
+    // 管理者可參訪的頁面
+    $adminUserRank = new UserRank(UserRank::ADMIN);
+	$authSystem->redirectHomeWhenBelowRank($loginUserRank, $adminUserRank);
+    
+    // 設定主版
+    $title = '新增設備型號';
+	$navContentPath = 'contents/nav_admin.php';
+	$contentPath = 'contents/editmodel.php';
+	$addScripts = array();
+	
+    // 設定頁面內文
+    $modelData = array('model' => '',
+					   'category_id' => '');
+    $submitValue = '新增設備型號';
+	$postUrl = 'newmodel.php';
+	$postDeleteImageUrl = 'newmodel.php';
+	$errors = array();
+	$infos = array();
+	$categoryModel = new CategoryModel();
+	$categories = $categoryModel->get();
+	$image = NULL;
+	
+	// 處理刪除現有圖片情況
+	if (isset($_POST['delete_image_id'])) {
+		$fileManagement = new FileManagement();
+		if ($fileManagement->delete_file($_POST['delete_image_id'])) {
+			$infos[] = '圖片刪除成功';
+		} else {
+			$infos[] = '圖片刪除失敗';
+		}
+	}
+	// 處理新增型號資料
+	elseif (isset($_POST['model']) and isset($_POST['category_id'])) {
+		$validator = new ModelValidator();
+		$model = trim($_POST['model']);
+		$categoryId = trim($_POST['category_id']);
+		$errors = array_merge($errors, $validator->validateForAdd($model, $categoryId));
+		// 驗證上傳圖片
+		if (isset($_FILES['image']) and $_FILES['image']['error'] != UPLOAD_ERR_NO_FILE) {
+			if ($_FILES['image']['error'] == UPLOAD_ERR_OK) {
+				$imageName = $_FILES['image']['name'];
+				$imageTmpPath = $_FILES['image']['tmp_name'];
+			} else {
+				$imageError = new FileError($_FILES['image']['error']);
+				$errors[] = $imageError->get_error_message();
+			}
+		}
+		// 資料驗證無錯誤，嘗試新增資料
+		if (!$errors) {
+			$modelModel = new ModelModel();
+			if ($modelId = $modelModel->addModel($categoryId, $model)) {
+				$infos[] = '設備型號新增成功';
+				// 新增圖片
+				if (isset($imageName)) {
+					$fileManagement = new FileManagement();
+					$fileId = $fileManagement->save_file($imageName, $imageTmpPath);
+					if ($fileId !== False) {
+						$modelModel->addModelImageById($modelId, $fileId);
+					} else {
+						$errors[] = '圖片上傳失敗';
+					}
+				}
+			} else {
+				$errors[] = '發生不知名錯誤，請通知管理員';
+			}
+			
+		}
+		// 資料驗證錯誤 
+		else {
+			$modelData['model'] = $model;
+			$modelData['category_id'] = $categoryId;
+		}
+	}
+	
+	// 載入主版
+    require_once 'templates/layout.php';
+?>
