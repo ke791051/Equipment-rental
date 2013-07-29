d1bb59efe1602f923c6ab925f0afcddbeb8b435d
Yuo <s11977037@gms.nutc.edu.tw>
2013-07-29 10:03:41 +0800

    新增顯示訊息的content

---
+++ contents/message.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * anticipate:
+ *  - string $header
+ *  - array $infos
+ *  - array $errors
+ *  - string $redirectUrl
+ */
+?>
+
+<h1><?php echo $header ?></h1>
+
+<ul>
+	<?php foreach($infos as $info): ?>
+		<li><?php echo $info ?></li>
+	<?php endforeach ?>
+</ul>
+
+<ul>
+	<?php foreach($errors as $error): ?>
+		<li><?php echo $error ?></li>
+	<?php endforeach ?>
+</ul>
+
+<a href="<?php echo $redirectUrl ?>">返回</a>
