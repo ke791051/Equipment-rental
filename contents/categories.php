c7730002dd832bcb34abc9ac932458708a68de15
Yuo <s11977037@gms.nutc.edu.tw>
2013-07-29 10:02:42 +0800

    新增設備型號管理頁面

---
+++ contents/categories.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * anticipate:
+ *  - array $categories 格式符合Model回傳的型號分類格式
+ *  - string $navigateUrl 導覽網址，假設分頁資料是用GET方法傳送，格式為?perpage=int&page=int
+ *  - string $postEditUrl 編輯操作的post連結，使用id為名稱傳送資料庫編號
+ *  - string $postDeleteUrl 刪除操作的post連結，使用id為名稱傳送資料庫編號
+ *  - array $operators 是否要顯示指定操作，格式為array('delete' => boolean, 'edit' => boolean)
+ *  - int $perPage 一頁顯示多少筆資料
+ *  - int $page 目前的頁數
+ *  - int $totalPages 全部頁數
+ *  
+ */
+?>
+
+<h1>設備分類</h1>
+
+<table border="3">
+	<thead>
+		<th>設備分類名稱</th>
+		<?php if (in_array(True, $operators)): ?>
+			<th>操作</th>
+		<?php endif ?>
+	</thead>
+	<tbody>
+		<?php foreach ($categories as $category): ?>
+			<tr>
+				<td><?php echo $category['name'] ?></td>
+				<?php if (in_array(True, $operators)): ?>
+					<td>
+						<?php if ($operators['edit']): ?>
+							<form action="<?php echo $postEditUrl ?>" method="post">
+								<input type="hidden" name="id" value="<?php echo $category['id'] ?>" />
+								<input type="hidden" name="postfromurl" value="<?php echo $navigateUrl . '?' . http_build_query(array('page' => $page, 'perpage' => $perpage)) ?>" />
+								<input type="submit" value="編輯" />
+							</form>
+						<?php endif ?>
+						<?php if ($operators['delete']): ?>
+							<form action="<?php echo $postDeleteUrl ?>" method="post">
+								<input type="hidden" name="id" value="<?php echo $category['id'] ?>" />
+								<input type="hidden" name="postfromurl" value="<?php echo $navigateUrl . '?' . http_build_query(array('page' => $page, 'perpage' => $perpage)) ?>" />
+								<input type="submit" value="刪除" />
+							</form>
+						<?php endif ?>
+					</td>
+				<?php endif ?>
+			</tr>
+		<?php endforeach ?>
+	</tbody>
+	<tfoot>
+		<p>
+			<?php if ($totalPages != 1): ?>
+				<?php if ($page != 1): ?>
+					<a href="<?php echo $navigateUrl . '?' . http_build_query(array('page' => 1, 'perpage' => $perpage)) ?>">第一頁</a>
+					<a href="<?php echo $navigateUrl . '?' . http_build_query(array('page' => $page - 1, 'perpage' => $perpage))?>">上一頁</a>
+				<?php endif ?>
+				<?php if ($page != $totalPages): ?>
+					<a href="<?php echo $navigateUrl . '?' . http_build_query(array('page' => $page + 1, 'perpage' => $perpage))?>">下一頁</a>
+					<a href="<?php echo $navigateUrl . '?' . http_build_query(array('page' => $totalPages, 'perpage' => $perpage))?>">最末頁</a>
+				<?php endif ?>
+			<?php endif ?>
+		</p>
+	</tfoot>
+</table>
---
+++ managecategories.php
@@ -0,0 +1,45 @@
+<?php
+	require_once 'config.php';
+	
+    // 驗證使用者
+    $authSystem = new AuthSystem();
+	$loginSystem = new LoginSystem();
+	$loginUserRank = $loginSystem->getLoginUserRank();
+	if (is_null($loginUserRank)) {
+		$authSystem->redirectHome();
+		exit();
+	}
+	$adminUserRank = new UserRank(UserRank::ADMIN);
+	$authSystem->redirectHomeWhenBelowRank($loginUserRank, $adminUserRank);
+    
+    // 設定主版
+    $title = '設備型號管理';
+	$navContentPath = 'contents/nav_admin.php';
+	$contentPath = 'contents/categories.php';
+	$addScripts = array();
+    
+    // 設定頁面內容
+	$navigateUrl = 'managecategories.php';
+	$postEditUrl = 'editcategory.php';
+	$postDeleteUrl = 'deletecategory.php';
+	$operators = array('delete' => True,
+					   'edit' => True);
+	if (isset($_GET['perpage'])) {
+		$perpage = (int) $_GET['perpage'];
+		$perpage = $perpage > 0 ? $perpage : $config['DEFAULT_PERPAGE'];
+	} else {
+		$perpage = $config['DEFAULT_PERPAGE'];
+	}
+	if (isset($_GET['page'])) {
+		$page = (int) $_GET['page'];
+		$page = $page > 0 ? $page : $config['DEFAULT_PAGE'];
+	} else {
+		$page = $config['DEFAULT_PAGE'];
+	}
+	$categoryModel = new CategoryModel();
+	$categories = $categoryModel->get($perpage, ($page - 1) * $perpage); // page is 1-based
+    $totalItems = $categoryModel->getCount();
+	$totalPages = ceil($totalItems / $perpage);
+	
+    require_once 'templates/layout.php'; // May use two-nav layout
+?>
