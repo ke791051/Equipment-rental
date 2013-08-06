558bd8804ce46a047ec246a31546f3f29395e701
Yuo <s11977037@gms.nutc.edu.tw>
2013-08-06 15:20:14 +0800

    - 新增myhistory 功能 - 新增myregisters 功能

---
+++ myhistory.php
@@ -0,0 +1,56 @@
+<?php
+require_once 'config.php';
+// 驗證使用者是否登入
+$authSystem = new AuthSystem();
+$loginSystem = new LoginSystem();
+$loginUserId = $loginSystem->getLoginUserId();
+$loginUserRank = $loginSystem->getLoginUserRank();
+if (is_null($loginUserId)) {
+	$authSystem->redirectHome();
+}
+
+// 設定主版資料
+$title = '我借的設備';
+$navContentPath = $loginUserRank->isEqual(new UserRank(UserRank::ADMIN)) ? 'contents/nav_admin.php' : 'contents/nav_user.php';
+$contentPath = 'contents/history.php';
+$addScripts = array();
+
+// 設定頁面資料
+$lendModel = new LendModel();
+$instanceModel = new InstanceModel();
+$modelModel = new ModelModel();
+$categoryModel = new CategoryModel();
+
+$caption = $title;
+$postLendBackUrl = '';
+$operators = array('lendBack' => False);
+$navigateUrl = $config['BASE_PATH'] . 'myhistory.php';
+
+// 設定分頁資料
+$getData = filter_input_array(INPUT_GET, array('perpage' => FILTER_VALIDATE_INT, 'page' => FILTER_VALIDATE_INT));
+$perpage = (int) $getData['perpage'];
+$page = (int) $getData['page'];
+$perpage = $perpage > 0 ? $perpage : $config['DEFAULT_PERPAGE'];
+$totalPages = ceil($lendModel->getCount() / $perpage);
+$page = ($page > 0 and $page <= $totalPages) ? $page : $config['DEFAULT_PAGE'];
+
+// 載入Model資料
+$lendsModelData = $lendModel->get($perpage, ($page - 1) * $perpage);
+$lends = array();
+foreach ($lendsModelData as $lendModelData) {
+	$lend = array();
+	$lend['lend'] = $lendModelData;
+	
+	$lend['instance'] = $instanceModel->getById($lend['lend']['instances_id']);
+	
+	$lend['model'] = $modelModel->getById($lend['instance']['model_id']);
+	
+	$lend['category'] = $categoryModel->getById($lend['model']['category_id']);
+	
+	$lend['user'] = array('name' => 'Not Implemented', 'sy' => 'Not Implemented');
+	
+	$lends[] = $lend;
+}
+
+require_once 'templates/layout.php';
+// End of file
---
+++ myregisters.php
@@ -0,0 +1,54 @@
+<?php
+require_once 'config.php';
+// 檢查使用者是否登入
+$authSystem = new AuthSystem();
+$loginSystem = new LoginSystem();
+$loginUserId = $loginSystem->getLoginUserId();
+if (is_null($loginUserId)) {
+	$authSystem->redirectHome();
+}
+
+// 設定主版資料
+$title = '我的申請紀錄';
+$loginUserRank = $loginSystem->getLoginUserRank();
+$navContentPath = $loginUserRank->isEqual(new UserRank(UserRank::ADMIN)) ? 'contents/nav_admin.php' : 'contents/nav_user.php';
+$contentPath = 'contents/registers.php';
+$addScripts = array();
+
+// 設定頁面資料
+$caption = $title;
+$navigateUrl = $config['BASE_PATH'] . 'myregisters.php';
+$operators = array('verify' => False);
+// 載入Model
+$registerModel = new RegisterModel();
+$instanceModel = new InstanceModel();
+$modelModel = new ModelModel();
+$categoryModel = new CategoryModel();
+
+// 設定分頁資料
+$getData = filter_input_array(INPUT_GET, array('perpage' => FILTER_VALIDATE_INT, 'page' => FILTER_VALIDATE_INT));
+$page = (int) $getData['page'];
+$perpage = (int) $getData['perpage'];
+$perpage = $perpage > 0 ? $perpage : $config['DEFAULT_PERPAGE'];
+$totalPages = ceil($registerModel->getCount() / $perpage);
+$page = ($page > 0 and $page <= $totalPages) ? $page : $config['DEFAULT_PAGE'];
+// 設定Model資料
+$registers = array();
+foreach ($registerModel->get($perpage, ($page - 1) * $perpage) as $register) {
+	$modelData = array();
+	
+	$modelData['register'] = $register;
+	
+	$modelData['instance'] = $instanceModel->getById($modelData['register']['instances_id']);
+	
+	$modelData['model'] = $modelModel->getById($modelData['instance']['model_id']);
+	
+	$modelData['category'] = $categoryModel->getById($modelData['model']['category_id']);
+	
+	$modelData['user'] = array('name' => 'Not Implemented', 'sy' => 'Not Implemented');
+	
+	$registers[] = $modelData;
+}
+
+require_once 'templates/layout.php';
+// End of file
