4e49958cdfa8875991bd322dd3c2f62dd77988dc
Yuo <s11977037@gms.nutc.edu.tw>
2013-08-06 15:10:18 +0800

    新增Layout CSS

--- templates/layout.php
+++ templates/layout.php
@@ -12,6 +12,8 @@
     <head>
     	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title><?php echo $title ?></title>
+        <link rel="stylesheet" href="css/reset.css" type="text/css" charset="utf-8" />
+    	<link rel="stylesheet" href="css/layout.css" type="text/css" charset="utf-8" />
         <?php
             foreach ($addScripts as $addScript):
                 echo $addScript;
@@ -19,15 +21,60 @@
         ?>
     </head>
     <body>
-        <div id="header">
-        </div>
-        <div id="nav">
-            <?php include_once $navContentPath ?>
-        </div>
-        <div id="content">
-            <?php include_once $contentPath ?>
-        </div>
-        <div id="footer">
-        </div>
+    	<div id="page">
+	        <div id="header">
+	        		<span id="infos">
+	
+	                <img src="images/User Anonymous Yellow Regular_256.png" height="24.3px" width="18.6px"><a href="index.php">系統管理員及會員登入</a>
+	
+					</span>
+	
+					<a href="index.html" id="logo"></a> <!-- /#logo -->
+	
+	                <center>
+	
+					<ul id="navigation"> 
+	
+						<?php include_once $navContentPath ?>	
+	
+					</ul>
+	        </div> <!-- /#header -->
+	        
+	        <div id="content">
+	        	<div id="main">
+	            	<?php include_once $contentPath ?>
+	            </div>
+	        </div>
+	        
+			<div id="footer">
+				<div id="description">
+					<div id="Copyright">
+						<a href="index.html" class="logo"></a>
+						<span>&copy; Copyright &copy; 2013. <a href="index.html">Company name</a>All rights reserved</span>
+					</div>
+					<div id="summary">
+                        <p>透過本系統，不僅可以縮短查詢資料的時間，使用者與管理者對於設備出借狀況一目了然，再透過電腦key in借用資料，省去人工對於傳統出借單不知如何寫起與漏寫的種種問題；再者，一本又一本的出借單究竟是要留著還是丟掉?實在是令人頭疼，因此開發此系統，已達到數位化管理。
+    
+                  歡迎使用本系統!! </p>
+              		</div>
+				</div>
+				<div class="navigation">
+                	<ul>
+                    	<li>
+                        	<a href="index.html">系統簡介</a>
+                        </li>
+                        <li>
+                        	<a href="http://www.nutc.edu.tw">臺中科技大學</a>
+                        </li>
+                        <li>
+                        	<a href="http://im.nutc.edu.tw">資訊管理系</a>
+                        </li>
+                        <li>
+                        	<a href="register.php">註冊會員</a>
+                        </li>
+                    </ul>
+                </div>
+			</div> <!-- /#footer -->
+        </div> <!-- /#page -->
     </body>
 </html>
