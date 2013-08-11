7b4bd55c34a10b40851d08ca9e6e0d9f639a2c07
Yuo <s11977037@gms.nutc.edu.tw>
2013-08-11 15:55:30 +0800

    建立管理者和使用者的導覽列

--- contents/nav_admin.php
+++ contents/nav_admin.php
@@ -1,3 +1,8 @@
-<?php
-    //phpinfo();
-?>
+<li><a href="manageregisters.php">審核</a></li>
+<li><a href="manageinstances.php">設備管理</a></li>
+<li><a href="managemodels.php">設備型號管理</a></li>
+<li><a href="managecategories.php">設備分類管理</a></li>
+<li><a href="managelends.php">設備出借管理</a></li>
+<li><a href="newinstance.php">新增設備</a></li>
+<li><a href="newmodel.php">新增設備型號</a></li>
+<li><a href="newcategory.php">新增設備分類</a></li>
