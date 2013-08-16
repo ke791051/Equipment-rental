2d0dde3be50b17c941b56ca9648f21d1ca21ac9d
Yuo <s11977037@gms.nutc.edu.tw>
2013-08-16 13:23:53 +0800

    導覽列修改

--- contents/nav_admin.php
+++ contents/nav_admin.php
@@ -3,6 +3,8 @@
 <li><a href="managemodels.php">設備型號管理</a></li>
 <li><a href="managecategories.php">設備分類管理</a></li>
 <li><a href="managelends.php">設備出借管理</a></li>
+<li><a href="manageusers.php">會員管理</a></li>
 <li><a href="newinstance.php">新增設備</a></li>
 <li><a href="newmodel.php">新增設備型號</a></li>
 <li><a href="newcategory.php">新增設備分類</a></li>
+<li><a href="newuser.php">新增會員</a></li>
