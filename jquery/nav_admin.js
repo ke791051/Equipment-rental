4fed2be42fc9dddc3557b514f22fa9f97853b33a
Yuo <s11977037@gms.nutc.edu.tw>
2013-08-23 14:26:06 +0800

    修復導覽列樣式問題

--- jquery/nav_admin.js
+++ jquery/nav_admin.js
@@ -1,8 +1,9 @@
 // JavaScript Document
 $(function(){
 	$('#device').click(function(){
-		$(this).css('background','rgba(255,255,255,0.4)');
-		$('#device_list').slideDown(100);
+		$('#device_list').slideDown(100, function(){
+			$('#device').addClass('selected');
+		});
 	})
 	$(document).mouseup(function(event){
 		if($(event.target).parent('#device_list').length==0)
@@ -10,6 +11,7 @@ $(function(){
 			$('#device_list').slideUp(100
 			,function(){
 				$('#device_list').hide();
+				$('.selected').removeClass('selected');
 			});
 		}
 	})
